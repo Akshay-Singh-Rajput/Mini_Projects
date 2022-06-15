8b3e63fbdbc89babc02009e20fadd81b4e9537a6
Akshay-Singh-Rajput <akshaysinghr98@gmail.com>
2022-06-15 19:06:09 +0530

    collapse

---
+++ React/Collapse/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Collapse } from './Collapse';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Collapse>
+        <h1>This is a collapse</h1>
+        <p>Hello world!</p>
+      </Collapse>
+    </div>
+  )
+}
+
+export default App
